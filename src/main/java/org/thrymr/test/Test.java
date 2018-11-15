951fb2fd04b534a375a5587cae104f086e725d25
Sourabh <sourav.rawat@thrymr.net>
2018-11-15 17:35:18 +0530

    Using the branch

--- src/main/java/org/thrymr/test/Test.java
+++ src/main/java/org/thrymr/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
 public static void main(String[] args) {
 	System.out.println("this is the test class");
     System.out.println("hi this io sourabh singh rawat ");
+    System.out.println("use ofn the branch");
 }
 }
