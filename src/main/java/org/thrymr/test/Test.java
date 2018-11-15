bcdcc9fe31eb92250a424b8b14c8c3705c509b37
Sourabh <sourav.rawat@thrymr.net>
2018-11-15 16:51:34 +0530

    add one new Println Statement

---
+++ src/main/java/org/thrymr/test/Test.java
@@ -0,0 +1,8 @@
+package org.thrymr.test;
+
+public class Test {
+public static void main(String[] args) {
+	System.out.println("this is the test class");
+    System.out.println("hi this io sourabh singh rawat ");
+}
+}
