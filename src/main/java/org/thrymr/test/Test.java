8a7916979137c170e2cbf4e7bdada23174c925a9
neelesh-thrymr <45037093+neelesh-thrymr@users.noreply.github.com>
2018-11-15 17:38:37 +0530

    file deleted

--- src/main/java/org/thrymr/test/Test.java
+++ src/main/java/org/thrymr/test/Test.java
@@ -2,7 +2,7 @@ package org.thrymr.test;
 
 public class Test {
 public static void main(String[] args) {
-	System.out.println("this is the test class");
+
     System.out.println("hi this io sourabh singh rawat ");
     System.out.println("use ofn the branch");
 }
